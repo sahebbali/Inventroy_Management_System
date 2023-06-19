edf5a4433d2cdcee74d61f16e4b1ce6aaa2177e0
sahebbali <sahebbali253@gmail.comm>
2023-06-19 22:19:50 +0600

    finish BackEnd

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,19 @@
 const express =require('express');
-const UsersController =require('../controllers/Users/UsersController')
+const AuthVerifyMiddleware=require("../middlewares/AuthVerifyMiddleware");
+const UsersController=require("../controllers/Users/UsersController");
 const BrandsController=require("../controllers/Brands/BrandsController");
 const CategoriesController = require("../controllers/Categories/CategoriesController");
 const CustomersController = require("../controllers/Customers/CustomersController");
+const SuppliersController = require("../controllers/Suppliers/SuppliersController");
+const ExpenseTypesController = require("../controllers/Expenses/ExpenseTypesController");
+const ExpensesController = require("../controllers/Expenses/ExpensesController");
+const ProductsController = require("../controllers/Products/ProductsController");
 const PurchasesController = require("../controllers/Purchases/PurchasesController");
+const SalesController = require("../controllers/Sales/SalesController");
 const ReturnsController = require("../controllers/Returns/ReturnsController");
+const ReportController = require("../controllers/Report/ReportController");
+const SummaryController = require("../controllers/Summary/SummaryController");
 
-const AuthVerifyMiddleware =require('../middlewares/AuthVerifyMiddleware')
 const router =express.Router();
 
 
@@ -29,7 +36,6 @@ router.get("/DeleteBrand/:id",AuthVerifyMiddleware,BrandsController.DeleteBrand)
 router.get("/BrandDetailsByID/:id",AuthVerifyMiddleware,BrandsController.BrandDetailsByID);
 
 
-
 // Categories
 router.post("/CreateCategories",AuthVerifyMiddleware,CategoriesController.CreateCategories);
 router.post("/UpdateCategories/:id",AuthVerifyMiddleware,CategoriesController.UpdateCategories);
@@ -49,16 +55,72 @@ router.get("/CustomersDetailsByID/:id",AuthVerifyMiddleware,CustomersController.
 
 
 
+
+// Suppliers
+router.post("/CreateSuppliers",AuthVerifyMiddleware,SuppliersController.CreateSuppliers);
+router.post("/UpdateSuppliers/:id",AuthVerifyMiddleware,SuppliersController.UpdateSuppliers);
+router.get("/SuppliersList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,SuppliersController.SuppliersList);
+router.get("/SuppliersDropDown",AuthVerifyMiddleware,SuppliersController.SuppliersDropDown);
+router.get("/DeleteSupplier/:id",AuthVerifyMiddleware,SuppliersController.DeleteSupplier);
+router.get("/SuppliersDetailsByID/:id",AuthVerifyMiddleware,SuppliersController.SuppliersDetailsByID);
+
+
+// ExpenseTypes
+router.post("/CreateExpenseTypes",AuthVerifyMiddleware,ExpenseTypesController.CreateExpenseTypes);
+router.post("/UpdateExpenseTypes/:id",AuthVerifyMiddleware,ExpenseTypesController.UpdateExpenseTypes);
+router.get("/ExpenseTypesList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,ExpenseTypesController.ExpenseTypesList);
+router.get("/ExpenseTypesDropDown",AuthVerifyMiddleware,ExpenseTypesController.ExpenseTypesDropDown);
+router.get("/DeleteExpenseTypes/:id",AuthVerifyMiddleware,ExpenseTypesController.DeleteExpenseTypes);
+router.get("/ExpenseTypesDetailsByID/:id",AuthVerifyMiddleware,ExpenseTypesController.ExpenseTypesDetailsByID);
+
+
+// Expenses
+router.post("/CreateExpenses",AuthVerifyMiddleware,ExpensesController.CreateExpenses);
+router.post("/UpdateExpenses/:id",AuthVerifyMiddleware,ExpensesController.UpdateExpenses);
+router.get("/ExpensesList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,ExpensesController.ExpensesList);
+router.get("/DeleteExpense/:id",AuthVerifyMiddleware,ExpensesController.DeleteExpense);
+router.get("/ExpenseDetailsByID/:id",AuthVerifyMiddleware,ExpensesController.ExpenseDetailsByID);
+
+
+// Products
+router.post("/CreateProducts",AuthVerifyMiddleware,ProductsController.CreateProducts);
+router.post("/UpdateProducts/:id",AuthVerifyMiddleware,ProductsController.UpdateProducts);
+router.get("/ProductsList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,ProductsController.ProductsList);
+router.get("/DeleteProduct/:id",AuthVerifyMiddleware,ProductsController.DeleteProduct);
+router.get("/ProductsDetailsByID/:id",AuthVerifyMiddleware,ProductsController.ProductsDetailsByID);
+router.get("/ProductsDropDown",AuthVerifyMiddleware,ProductsController.ProductsDropDown);
+
+
 //Purchases
 router.post("/CreatePurchases",AuthVerifyMiddleware,PurchasesController.CreatePurchases);
 router.get("/PurchasesList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,PurchasesController.PurchasesList);
 router.get("/PurchasesDelete/:id",AuthVerifyMiddleware,PurchasesController.PurchasesDelete);
 
 
+//Sales
+router.post("/CreateSales",AuthVerifyMiddleware,SalesController.CreateSales);
+router.get("/SalesList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,SalesController.SalesList);
+router.get("/SaleDelete/:id",AuthVerifyMiddleware,SalesController.SaleDelete);
+
+
 //Return
 router.post("/CreateReturns",AuthVerifyMiddleware,ReturnsController.CreateReturns);
 router.get("/ReturnsList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,ReturnsController.ReturnsList);
 router.get("/ReturnDelete/:id",AuthVerifyMiddleware,ReturnsController.ReturnDelete);
+
+
+//Report
+router.post("/ExpensesByDate",AuthVerifyMiddleware,ReportController.ExpensesByDate);
+router.post("/ReturnByDate",AuthVerifyMiddleware,ReportController.ReturnByDate);
+router.post("/PurchaseByDate",AuthVerifyMiddleware,ReportController.PurchaseByDate);
+router.post("/SalesByDate",AuthVerifyMiddleware,ReportController.SalesByDate);
+
+
+//Summary
+router.get("/ExpensesSummary",AuthVerifyMiddleware,SummaryController.ExpensesSummary);
+router.get("/ReturnSummary",AuthVerifyMiddleware,SummaryController.ReturnSummary);
+router.get("/PurchaseSummary",AuthVerifyMiddleware,SummaryController.PurchaseSummary);
+router.get("/SalesSummary",AuthVerifyMiddleware,SummaryController.SalesSummary);
 
 
 module.exports=router;
